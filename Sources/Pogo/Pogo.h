1ee4819a0f5b83aeee4f641906f460e666a20b62
Alex Basson <alexbasson@gmail.com>
2016-01-26 07:16:31 -0500

    Initial project setup

---
+++ Sources/Pogo/Pogo.h
@@ -0,0 +1,19 @@
+//
+//  Pogo.h
+//  Pogo
+//
+//  Created by Alex on 1/26/16.
+//  Copyright © 2016 Conditionally Convergent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pogo.
+FOUNDATION_EXPORT double PogoVersionNumber;
+
+//! Project version string for Pogo.
+FOUNDATION_EXPORT const unsigned char PogoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pogo/PublicHeader.h>
+
+
